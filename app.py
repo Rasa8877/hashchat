bacf0d8a9cc764f56a46cad7f1ebb931dc787aae
Rasa Dariush <letperhut@gmail.com>
2026-01-03 00:31:31 +0330

    Small change

--- app.py
+++ app.py
@@ -105,4 +105,6 @@ def on_leave(data):
     emit("msg", {"nick":"SYSTEM","msg":f'{data["nick"]} left.'}, room=data["room"])
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    socketio.run(app, host="0.0.0.0", port=port)
